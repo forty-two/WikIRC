a4818c1e157036be5671409c6d31374c2618d41a
Abram A <abram@forty-two.nu>
2012-01-21 10:00:32 +1100

    Initial commit

---
+++ permissions.py
@@ -0,0 +1,69 @@
+#! /usr/bin/python
+
+import os
+import json
+
+
+class AuthHandler():
+	def __init__(self, filename):
+		self.filename = filename
+		self.config = {}
+
+	def add_user(self, username, group, hostmask):
+		username = username.lower()
+		group = group.lower()
+
+		self.config[username] = {'groups'    : [group],
+								 'hostmasks': [hostmask]
+								 }
+		self._save_config()
+	
+	def add_user_hostmask(self, username, hostmask):
+		username = username.lower()
+		self.config['username']['hostmasks'].append(hostmask)
+		self._save_config()
+
+	def remove_user_hostmask(self, username, hostmask):
+		username = username.lower()
+		try:
+			self.config[username]['hostmasks'].pop(hostmask)
+			self._save_config()
+		except KeyError:
+			return False	
+
+	def get_group_list(self):
+		groupList = []
+		for user in self.config:
+			groupList.extend([i for i in self.config[user]['groups'] if i not in groups])
+		return groupList
+
+	def get_user_permissions(self, username, hostmask):
+		knownHostmasks = self.config.get(username, {}).get('hostmasks', [])
+		if hostmask in knownHostmasks:
+			return self.config.get(username).get('groups')
+		else:
+			return False
+											
+	def remove_user(self, username):
+		username = username.lower()
+		self.config.pop(username)	
+
+	def _load_config(self):
+		try:
+			if os.path.isfile(self.filename):
+				self.config = json.load(open(self.filename))
+
+		except ValueError:
+			self.config = {}
+
+	def _save_config(self):
+		f = open(self.filename, 'w')
+		f.write(json.dumps(self.config, indent = 4))
+		f.close()
+
+
+	
+
+
+
+
